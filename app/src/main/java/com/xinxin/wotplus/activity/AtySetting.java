691c465c4510d665aee04f4f50a41e66ad3ccbef
xinxin <377241804@qq.com>
2016-04-12 21:28:07 +0800

    添加缓存的展示与清除

--- app/src/main/java/com/xinxin/wotplus/activity/AtySetting.java
+++ app/src/main/java/com/xinxin/wotplus/activity/AtySetting.java
@@ -1,12 +1,15 @@
 package com.xinxin.wotplus.activity;
 
 import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
+import android.support.design.widget.Snackbar;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
 import com.xinxin.wotplus.R;
 import com.xinxin.wotplus.base.BaseActivity;
-import com.xinxin.wotplus.fragment.SettingFragment;
+import com.xinxin.wotplus.util.DataClearManager;
 
 /**
  * Created by xinxin on 2016/4/10.
@@ -37,5 +40,40 @@ public class AtySetting extends BaseActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * 设置Fragment
+     */
+    public static class SettingFragment extends PreferenceFragment implements Preference.OnPreferenceClickListener {
+
+        private Preference mClearCache;
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.setting);
+
+            mClearCache = findPreference("clear_cache");
+            updateCache();
+            mClearCache.setOnPreferenceClickListener(this);
+        }
+
+        /**
+         * 更新缓存大小的展示
+         */
+        void updateCache() {
+            mClearCache.setSummary(DataClearManager.getApplicationDataSize(getActivity()));
+        }
+
+        @Override
+        public boolean onPreferenceClick(Preference preference) {
+            if (mClearCache == preference) {
+                DataClearManager.cleanApplicationData(getActivity());
+                updateCache();
+                Snackbar.make(getView(), "缓存已清除", Snackbar.LENGTH_SHORT).show();
+            }
+            return false;
+        }
     }
 }
