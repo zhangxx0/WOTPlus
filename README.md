0f02212bda4d7d80db2beddaaf13878c54148d91
xinxin <377241804@qq.com>
2016-04-25 22:55:33 +0800

    Readme basic info

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # WOTPlus
-坦克世界战绩查询APP
+_ _ _
+
+###前言
+
+WOTPlus是一款坦克世界（World of Tanks）战绩查询软件，旨在为国服坦克世界玩家提供一个方便且专业而又不失优美的战绩查询APP；
+
+作为一个坦克世界的老司机，我还是比较关心自己的战绩与排名，然而并没有一款好的app来让我实时的掌握我的战绩动态；
+
+与其等待，不如自己动手，且好几年过去了，并不见wot的app有些许的进步，国服对这个好像不怎么重视，于是，在学习了半年左右的Android之后，我动手来做人生第一个开源安卓项目，我想用可以接触到的最先进的安卓技术来开发这款APP，因为我觉得这是一件很酷的事情，而，上班以来，好久没有做过很酷的事情了。
+
+###截图
+
+![](http://xcc3641.qiniudn.com/app-%E5%B0%B1%E7%9C%8B%E5%A4%A9%E6%B0%94-%E9%97%AA%E5%B1%8F.png)
+
+
+###功能简介
+
+WOTPlus主要提供坦克世界战绩查询的功能，分为以下几个模块展示战绩信息：
+
+- 概要信息
+- 成就
+- 统计
+- 等级
+- 战车
+
+###技术阐述
+
+WOTPlus遵循**Material Design**设计原则，使用最新的控件，诸如：
+
+DrawerLayout、CoordinatorLayout、Toolbar、FloatingActionButton、NavigationView、RecyclerView、CardView等
+
+**数据源**：由于坦克世界官网的战绩查询页面并没有暴露json接口，所以我使用的是获取战绩页面html并使用jsoup解析数据，实属无奈之举，也造成查询时耗时较长；
+
+网络请求：Volley；
+
+图片加载：Glide；
+
+图表展示：MPAndroidChart；
+
+版本更新：Fir；
+
+第一版并没有使用架构诸如MVP、或者Rxjava等流行技术之类，别问我为什么，还没学会。。。
+因此编写之初就有了重构的计划，初步构想是使用Rxjava+Retrofit进行第二个版本的重构
+
+
--- app/src/main/java/com/xinxin/wotplus/MainActivity.java
+++ app/src/main/java/com/xinxin/wotplus/MainActivity.java
@@ -9,7 +9,6 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
@@ -189,12 +188,13 @@ public class MainActivity extends AppCompatActivity {
         c.startActivity(intent);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//
+//        // getMenuInflater().inflate(R.menu.menu_main, menu);
+//        return true;
+//    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
