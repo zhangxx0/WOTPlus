819b65d25812621a2a99cb4ab701b7c21224121d
zhangxx <zxx377241804@gmail.com>
2016-06-24 01:00:16 +0800

    add jpush

--- app/src/main/java/com/xinxin/wotplus/MyApplication.java
+++ app/src/main/java/com/xinxin/wotplus/MyApplication.java
@@ -2,11 +2,14 @@ package com.xinxin.wotplus;
 
 import android.app.Application;
 import android.content.Context;
+import android.util.Log;
+
+import cn.jpush.android.api.JPushInterface;
 
 /**
  * Created by xinxin on 2016/3/22.
  */
-public class MyApplication  extends Application {
+public class MyApplication extends Application {
 
     private static Context context;
 
@@ -14,6 +17,10 @@ public class MyApplication  extends Application {
     public void onCreate() {
         context = getApplicationContext();
         super.onCreate();
+        Log.d("+++++++++++++++++++++++", "初始化极光推送sdk");
+        // 初始化极光推送sdk
+        JPushInterface.setDebugMode(true);
+        JPushInterface.init(this);
     }
 
     public static Context getContext() {
